015bf460db058054172ae06f986d9baf733808e3
OmarMWarraich <earthling986@gmail.com>
2023-04-07 00:18:27 +0500

    feat: Client Connect & Disconnect

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,6 +4,17 @@ class Client {
     constructor() {
         this.socket = io();
 
+        this.socket.on('connect', function () {
+            console.log('Connected to server');
+            document.body.innerHTML = ''
+        });
+
+        this.socket.on('disconnect', function (message: any) {
+            console.log('Disconnected from server' + message);
+            document.body.innerHTML += 
+                'Disconnected from server' + message + '<br>';
+        });
+
         this.socket.on('message', function (message: any) {
             console.log(message);
             document.body.innerHTML += message + '<br>';
