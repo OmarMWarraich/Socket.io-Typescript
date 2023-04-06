b12feaec9551006287a812644cea8f9aa6d14fff
OmarMWarraich <earthling986@gmail.com>
2023-04-06 19:50:15 +0500

    feat: Server emit to Specific Socket

--- dist/server/server.js
+++ dist/server/server.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const http_1 = __importDefault(require("http"));
 const socket_io_1 = __importDefault(require("socket.io"));
+const luckyNumbersGame_1 = __importDefault(require("./luckyNumbersGame"));
 const port = 3000;
 class App {
     constructor(port) {
@@ -15,16 +16,27 @@ class App {
         app.use(express_1.default.static(path_1.default.join(__dirname, '../client')));
         this.server = new http_1.default.Server(app);
         this.io = new socket_io_1.default.Server(this.server);
+        this.game = new luckyNumbersGame_1.default();
         this.io.on('connection', (socket) => {
-            console.log('Client connected');
-            socket.emit('message', 'Hello from server' + socket.id);
+            console.log('Client connected: ' + socket.id);
+            this.game.LuckyNumbers[socket.id] = Math.floor(Math.random() * 10);
+            socket.emit('message', 'Hello from server' +
+                socket.id +
+                this.game.LuckyNumbers[socket.id]);
             socket.broadcast.emit('message', 'A new user has joined the chat' + socket.id);
             socket.on('disconnect', () => {
                 console.log('Client disconnected');
             });
         });
         setInterval(() => {
-            this.io.emit('random', Math.floor(Math.random() * 10));
+            let randomNumber = Math.floor(Math.random() * 10);
+            let winners = this.game.GetWinners(randomNumber);
+            if (winners.length > 0) {
+                winners.forEach((w) => {
+                    this.io.to(w).emit('message', 'You won!');
+                });
+            }
+            this.io.emit('random', randomNumber);
         }, 1000);
     }
     Start() {
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import http from 'http';
 import socketIO from 'socket.io';
+import LuckyNumbersGame from './luckyNumbersGame';
 
 const port: number = 3000;
 
@@ -10,6 +11,7 @@ class App {
     private port: number;
 
     private io: socketIO.Server;
+    private game: LuckyNumbersGame;
 
     constructor(port: number) {
         this.port = port;
@@ -20,10 +22,19 @@ class App {
         this.server = new http.Server(app);
         this.io = new socketIO.Server(this.server);
 
-        this.io.on('connection', (socket: socketIO.Socket) => {
-            console.log('Client connected');
+        this.game = new LuckyNumbersGame();
 
-            socket.emit('message', 'Hello from server' + socket.id);
+        this.io.on('connection', (socket: socketIO.Socket) => {
+            console.log('Client connected: ' + socket.id);
+
+            this.game.LuckyNumbers[socket.id] = Math.floor(Math.random() * 10);
+
+            socket.emit(
+                'message',
+                'Hello from server' +
+                 socket.id + 
+                 this.game.LuckyNumbers[socket.id]
+                 );
 
             socket.broadcast.emit('message', 'A new user has joined the chat'+ socket.id);
 
@@ -32,7 +43,14 @@ class App {
             });
         });
         setInterval(() => {
-            this.io.emit('random', Math.floor(Math.random() * 10))
+            let randomNumber: number = Math.floor(Math.random() * 10);
+            let winners: string[] = this.game.GetWinners(randomNumber);
+            if (winners.length > 0) {
+                winners.forEach((w) => {
+                    this.io.to(w).emit('message', 'You won!');
+                })
+            }
+            this.io.emit('random', randomNumber)
         }, 1000);
     }
 
